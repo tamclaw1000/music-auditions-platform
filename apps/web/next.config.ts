bdaf5fdc0df05c5ea76e705085abca0332509bbb
Man Wei Tam <manweitam@gmail.com>
2026-05-24 18:07:18 -0500

    Fix Pages routing for split sites

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -6,6 +6,7 @@ const isGithubActions = process.env.GITHUB_ACTIONS === 'true';
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   output: 'export',
+  trailingSlash: true,
   images: {
     unoptimized: true,
   },
